7891d424704a6e47a9aef56ee20770e688cf70db
Asmaa-Sh5 <asmaash342@gmail.com>
2024-08-16 04:21:47 +0300

    committ

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -12,10 +12,9 @@ router.post("/login", adminLogin);
 router.get("/users", getAllUsers);
 router.post(
   "/handle-authentication",
-  adminAuthMiddleware,
 
   handleAuthenticationRequest
 );
-router.delete("/user/:userId", adminAuthMiddleware, deleteUserAccount);
+router.delete("/user/:userId", deleteUserAccount);
 
 module.exports = router;
